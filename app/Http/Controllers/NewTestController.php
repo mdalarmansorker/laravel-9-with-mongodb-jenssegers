0d4096aea748e7d7f3797dda05719f461e28e438
Arman <mdalarmansorker@gmail.com>
2024-02-04 14:29:14 +0600

    laravel 9 with mongodb testing

---
+++ app/Http/Controllers/NewTestController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\New_test;
+use Illuminate\Http\Request;
+
+class NewTestController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\New_test  $new_test
+     * @return \Illuminate\Http\Response
+     */
+    public function show(New_test $new_test)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\New_test  $new_test
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(New_test $new_test)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\New_test  $new_test
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, New_test $new_test)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\New_test  $new_test
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(New_test $new_test)
+    {
+        //
+    }
+}
